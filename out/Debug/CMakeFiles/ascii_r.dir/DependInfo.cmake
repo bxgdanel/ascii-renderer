e0a5cf5fe5895ccab5b93cc0d7ebc94b08934073
bxgdanel <Bluca282@gmail.com>
2026-06-14 20:37:16 +0300

    Added CMake

---
+++ out/Debug/CMakeFiles/ascii_r.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/bogdanel/uhhhhh/ascii-renderer/domain.cpp" "CMakeFiles/ascii_r.dir/domain.cpp.o" "gcc" "CMakeFiles/ascii_r.dir/domain.cpp.o.d"
+  "/media/bogdanel/uhhhhh/ascii-renderer/main2.cpp" "CMakeFiles/ascii_r.dir/main2.cpp.o" "gcc" "CMakeFiles/ascii_r.dir/main2.cpp.o.d"
+  "/media/bogdanel/uhhhhh/ascii-renderer/perspectiverenderer.cpp" "CMakeFiles/ascii_r.dir/perspectiverenderer.cpp.o" "gcc" "CMakeFiles/ascii_r.dir/perspectiverenderer.cpp.o.d"
+  "/media/bogdanel/uhhhhh/ascii-renderer/renderer.cpp" "CMakeFiles/ascii_r.dir/renderer.cpp.o" "gcc" "CMakeFiles/ascii_r.dir/renderer.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ out/Debug/CMakeFiles/ascii_r.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ascii_r.dir/domain.cpp.o"
+  "CMakeFiles/ascii_r.dir/domain.cpp.o.d"
+  "CMakeFiles/ascii_r.dir/main2.cpp.o"
+  "CMakeFiles/ascii_r.dir/main2.cpp.o.d"
+  "CMakeFiles/ascii_r.dir/perspectiverenderer.cpp.o"
+  "CMakeFiles/ascii_r.dir/perspectiverenderer.cpp.o.d"
+  "CMakeFiles/ascii_r.dir/renderer.cpp.o"
+  "CMakeFiles/ascii_r.dir/renderer.cpp.o.d"
+  "ascii_r"
+  "ascii_r.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ascii_r.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
