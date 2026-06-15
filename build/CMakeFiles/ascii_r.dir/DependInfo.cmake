80824bd4fe20181347a17e0bb5958e1707f79a88
bxgdanel <Bluca282@gmail.com>
2026-06-15 20:32:56 +0300

    idk ig cmake windows

---
+++ build/CMakeFiles/ascii_r.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/lucab/Documents/ascii-renderer/Camera.cpp" "CMakeFiles/ascii_r.dir/Camera.cpp.obj" "gcc" "CMakeFiles/ascii_r.dir/Camera.cpp.obj.d"
+  "C:/Users/lucab/Documents/ascii-renderer/domain.cpp" "CMakeFiles/ascii_r.dir/domain.cpp.obj" "gcc" "CMakeFiles/ascii_r.dir/domain.cpp.obj.d"
+  "C:/Users/lucab/Documents/ascii-renderer/main2.cpp" "CMakeFiles/ascii_r.dir/main2.cpp.obj" "gcc" "CMakeFiles/ascii_r.dir/main2.cpp.obj.d"
+  "C:/Users/lucab/Documents/ascii-renderer/perspectiverenderer.cpp" "CMakeFiles/ascii_r.dir/perspectiverenderer.cpp.obj" "gcc" "CMakeFiles/ascii_r.dir/perspectiverenderer.cpp.obj.d"
+  "C:/Users/lucab/Documents/ascii-renderer/renderer.cpp" "CMakeFiles/ascii_r.dir/renderer.cpp.obj" "gcc" "CMakeFiles/ascii_r.dir/renderer.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ascii_r.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ascii_r.dir/Camera.cpp.obj"
+  "CMakeFiles/ascii_r.dir/Camera.cpp.obj.d"
+  "CMakeFiles/ascii_r.dir/domain.cpp.obj"
+  "CMakeFiles/ascii_r.dir/domain.cpp.obj.d"
+  "CMakeFiles/ascii_r.dir/main2.cpp.obj"
+  "CMakeFiles/ascii_r.dir/main2.cpp.obj.d"
+  "CMakeFiles/ascii_r.dir/perspectiverenderer.cpp.obj"
+  "CMakeFiles/ascii_r.dir/perspectiverenderer.cpp.obj.d"
+  "CMakeFiles/ascii_r.dir/renderer.cpp.obj"
+  "CMakeFiles/ascii_r.dir/renderer.cpp.obj.d"
+  "ascii_r.exe"
+  "ascii_r.exe.manifest"
+  "ascii_r.pdb"
+  "libascii_r.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ascii_r.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
